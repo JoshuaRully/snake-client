cc621e10b77eb8cc964e098e62acea534a8f3be5
Joshua Rully <joshrully@gmail.com>
2021-02-10 21:41:53 +0000

    fixed some input bugs, added message and movment

--- client.js
+++ client.js
@@ -1,15 +1,11 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
 const connect = function() {
   const conn = net.createConnection({ 
     password: 396035,
     host: '135.23.222.131',
     port: 50542
   });
-  // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log('Server says: ', data);
@@ -20,14 +16,12 @@ const connect = function() {
   conn.on('connect', () => {
     conn.write('Name: JMR');
   });
-
   // conn.on('connect', () => {
   //   setInterval(() => {
   //     conn.write('Move: left');   
   //   }, 50)
   // });
-
   return conn;
-}
+};
 
 module.exports = { connect };
--- input.js
+++ input.js
@@ -1,19 +1,34 @@
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
+let connection;
+const handUserInput = (data) => {
+  if (data === '\u0003') {
+    console.log("Ciao Bello!");
+    process.exit();
+  }
+  if (data === 'w') {
+    connection.write('Move: up');
+    }
+  if (data === 'a') {
+    connection.write('Move: left');
+  }
+  if (data === 's') {
+    connection.write('Move: down');
+  }
+  if (data === 'd') {
+    connection.write('Move: right');
+  }
+  if (data === 'r') {
+    connection.write('Say: wubbalubbadubdub')
+  }
+};
+const setupInput = function(conn) {
+  connection = conn
   const stdin = process.stdin;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', key => {
-    if (key === '\u0003') {
-      console.log("Ciao Bello!");
-      process.exit();
-    }
-  });
+  stdin.on('data', handUserInput)
   return stdin;
-}
+};
 
 module.exports = { setupInput };
